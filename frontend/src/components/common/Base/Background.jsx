36d1aa850b14ce848c32d91267f92c517041c2f3
Kaylanne <kaylannesatiro@icloud.com>
2026-06-21 15:25:12 -0300

    feat: criação da base do componente

--- frontend/src/components/common/Base/Background.jsx
+++ frontend/src/components/common/Base/Background.jsx
@@ -1,8 +1,14 @@
 import './Background.css'
 
-const Background = () => {
+const Background = ({ children }) => {
     return (
         <>
+            <div className="background-container">
+                <div className="background-mistico"></div>
+                <main className="conteudo-app">
+                    {children}
+                </main>
+            </div>
         </>
     )
 }
