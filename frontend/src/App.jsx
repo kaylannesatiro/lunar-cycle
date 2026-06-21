4576645585b9662609a55487f24cbbeecc48fda7
Kaylanne <kaylannesatiro@icloud.com>
2026-06-21 16:07:11 -0300

    chore: alteração do app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Menu from './components/common/Base/Menu'
 import Background from './components/common/Base/Background'
+import Button from './components/common/Buttons/Button'
 
 const App = () => {
   return (
@@ -11,6 +12,28 @@ const App = () => {
           { titulo: 'Calendário', rota: '/calendario' },
           { titulo: 'Conta', rota: '/conta' }
         ]} />
+
+        <Button 
+          variant="redondo" 
+          icone={<svg width="10" height="10" viewBox="0 0 10 10" fill="none"><path d="M5 0L10 5L5 10L0 5L5 0Z" fill="#E0C58F"/></svg>}
+          onClick={() => console.log("Registrando...")}
+        >
+          Registrar Menstruação
+        </Button>
+
+        <Button variant="padrao">
+          Salvar
+        </Button>
+
+        <Button variant="fechar" />
+
+        <Button variant="padrao" isLoading={true}>
+          Enviar
+        </Button>
+
+        <Button variant="padrao" color="#FF6B6B">
+          Excluir Sonho
+        </Button>
       </Background>
     </>
   )
