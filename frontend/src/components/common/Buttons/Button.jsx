8ff94d82d8206764ebec21496423b53e30b9ef71
Kaylanne <kaylannesatiro@icloud.com>
2026-06-21 16:36:18 -0300

    feat: add de background-color nos buttons

--- frontend/src/components/common/Buttons/Button.jsx
+++ frontend/src/components/common/Buttons/Button.jsx
@@ -1,18 +1,33 @@
 import "./Button.css"
 import { motion } from "framer-motion";
 
-const Button = ({variant = 'padrao', color, isLoading = false, onClick, disabled = false, children, icone}) => {
-
-    const classVariante = `btn-${variant}`;
+const Button = ({variant = 'padrao', backgroundColor, color, isLoading = false, onClick, disabled = false, children, icone}) => {
     const interativo = !disabled && !isLoading;
+    let estilosDeSeguranca = {};
+    
+    if (variant === 'padrao') {
+        estilosDeSeguranca = { width: '100%', maxWidth: '342px', height: '48px', minHeight: '48px' };
+    } else if (variant === 'redondo') {
+        estilosDeSeguranca = { width: '100%', maxWidth: '268px', height: '48px', minHeight: '48px' };
+    } else if (variant === 'fechar') {
+        estilosDeSeguranca = { width: '34px', height: '34px', minWidth: '34px', minHeight: '34px' };
+    }
+
+    if (color) {
+        estilosDeSeguranca.borderColor = color;
+        estilosDeSeguranca.color = color;
+    }
+
+    if (backgroundColor) {
+        estilosDeSeguranca.background = backgroundColor;
+    }
 
     return (
         <motion.button
-            className={`btn-base ${classVariante} ${!interativo ? 'btn-disabled' : ''}`}
+            className={`btn-base btn-${variant} ${!interativo ? 'btn-disabled' : ''}`}
             onClick={interativo ? onClick : undefined}
             disabled={!interativo}
-            
-            style={color ? { borderColor: color, color: color } : {}}
+            style={estilosDeSeguranca}
 
             whileHover={interativo ? { scale: 1.02, boxShadow: "0px 0px 12px rgba(224, 197, 143, 0.3)" } : {}}
             whileTap={interativo ? { scale: 0.95 } : {}}>
