9a05d7264d48cea86b9244ad47d0fa2e46cb2d77
Kaylanne <kaylannesatiro@icloud.com>
2026-06-21 15:35:26 -0300

    feat: inserção das estrelas no background

--- frontend/src/components/common/Base/Background.jsx
+++ frontend/src/components/common/Base/Background.jsx
@@ -1,10 +1,12 @@
 import './Background.css'
+import estrelasImg from '../../../assets/estrelas.png';
 
 const Background = ({ children }) => {
     return (
         <>
             <div className="background-container">
                 <div className="background-mistico"></div>
+                <div className="background-estrelas" style={{ backgroundImage: `url(${estrelasImg})` }}></div>
                 <main className="conteudo-app">
                     {children}
                 </main>
