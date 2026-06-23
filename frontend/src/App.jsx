b917d87cb81b705b7b215849441f615ff31b5ad0
Kaylanne <kaylannesatiro@icloud.com>
2026-06-23 12:54:29 -0300

    feat: inserção do footer no app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,3 +24,23 @@ const App = () => {
 }
 
 export default App
+
+/*
+Teste de muito conteúdo na página:
+<main className="main-content">
+          <h1 style={{color: 'white'}}>Bem-vindo ao Lunar Cycle</h1>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+          <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" alt="" style={{width:'600px'}}/>
+        </main>
+*/
