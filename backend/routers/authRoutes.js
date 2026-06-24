d545e0df7179085a79ea2539a88ea3c95120cb69
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-24 11:34:29 -0300

    feat: exponda rota

--- backend/routers/authRoutes.js
+++ backend/routers/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const validaRequisicao = require('../middlewares/validaRequisicao');
 const { criarContaSchema, loginSchema } = require('../schemas/authSchema');
+const {verificarToken} = require('../middlewares/autenticacao');
 
 
 //rotas de api/auth
@@ -14,6 +15,10 @@ router.post('/cadastro',
     
 //POST EM '/login' PARA REALIZAR LOGIN (HU-002)
 router.post('/login', validaRequisicao(loginSchema), authController.realizarLogin);
+
+//GET EM '/perfil' PARA OBTER DADOS DO PERFIL (HU-004)
+//privada, precisa de token JWT para acessar.
+router.get('/perfil', verificarToken, authController.obterDadosPerfil);
 
 
 module.exports = router;
