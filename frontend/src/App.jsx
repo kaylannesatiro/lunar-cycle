a409e16c3e24bbb67ae68c9ad15ac19dbcd82776
Kaylanne <kaylannesatiro@icloud.com>
2026-06-24 15:27:17 -0300

    chore: ajustes css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Menu from './components/common/Base/Menu'
 import Background from './components/common/Base/Background'
 import SelecaoSigno from './components/common/Inputs/SelecaoSigno'
 
@@ -11,11 +10,6 @@ const App = () => {
   return (
     <>
       <Background>
-        <Menu links={[
-          { titulo: 'Diário dos Sonhos', rota: '/diario' },
-          { titulo: 'Calendário', rota: '/calendario' },
-          { titulo: 'Conta', rota: '/conta' }
-        ]} />
 
         <div style={{ padding: '50px', backgroundColor: '#040612', minHeight: '100vh' }}>
             <SelecaoSigno 
