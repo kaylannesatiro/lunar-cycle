8ae18570975f1824a34beb5c975dd2e873eecc79
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-24 09:16:11 -0300

    feat: expondo rota POST de realizar login

--- backend/routers/authRoutes.js
+++ backend/routers/authRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const validaRequisicao = require('../middlewares/validaRequisicao');
-const { criarContaSchema } = require('../schemas/authSchema');
+const { criarContaSchema, loginSchema } = require('../schemas/authSchema');
 
-//POST EM '/cadastro' PARA CRIAR CONTA
+//POST EM '/cadastro' PARA CRIAR CONTA (HU-001)
 router.post('/cadastro',
     validaRequisicao(criarContaSchema),
     authController.criarConta);
     
+//POST EM '/login' PARA REALIZAR LOGIN (HU-002)
+router.post('/login', validaRequisicao(loginSchema), authController.realizarLogin);
+
+
 module.exports = router;
