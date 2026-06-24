57a4b52ce524f5054b23bce8d1978c5a2c6aa6ae
Kaylanne <kaylannesatiro@icloud.com>
2026-06-24 15:28:56 -0300

    chore: limpeza do app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import Background from './components/common/Base/Background'
-import SelecaoSigno from './components/common/Inputs/SelecaoSigno'
 
 const App = () => {
-
-  const [signoEscolhido, setSignoEscolhido] = useState('');
-
   return (
     <>
-      <Background>
-
-        <div style={{ padding: '50px', backgroundColor: '#040612', minHeight: '100vh' }}>
-            <SelecaoSigno 
-                value={signoEscolhido} 
-                onChange={setSignoEscolhido} 
-            />
-        </div>
-
-      </Background>
     </>
   )
 }
