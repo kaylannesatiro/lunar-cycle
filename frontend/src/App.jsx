3b0478c42e07fe7ce12e699dd3335a173919ed3e
Kaylanne <kaylannesatiro@icloud.com>
2026-06-25 17:14:41 -0300

    feat: coloando o componente no app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
 import Background from './components/common/Base/Background';
+import OraculoLunar from './components/features/Ciclo/CardOraculo';
 
 function App() {
+
+  const diaDoCicloDaUsuaria = 14; 
+  const estaSangrando = false; 
+
     return (
       <Background>
+        <OraculoLunar 
+          diaDoCiclo={diaDoCicloDaUsuaria} 
+          estaMenstruada={estaSangrando} 
+        />
       </Background>
     );
 }
