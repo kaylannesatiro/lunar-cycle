96b6828fc5e60232b9aec5d50927f2f979115020
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-26 10:35:28 -0300

    feat: criacao do controller ciclo com metodos alternarmenstruacao e obterdados

--- backend/controllers/cicloController.js
+++ backend/controllers/cicloController.js
@@ -1,2 +1,33 @@
 const cicloService = require('../services/cicloService');
 
+const obterDadosHome = async (req, res) => {
+    try{
+        const usuariaId = req.usuariaId; // Middleware de autenticação adiciona o id da usuária no objeto de requisição
+        const dadosHome = await cicloService.obterDadosHome(usuariaId);
+        return res.status(200).json(dadosHome);
+    } catch (erro) {
+        return res.status(500).json({
+            erro: "ERRO_BUSCA_HOME"
+        });
+    }
+};
+
+const alternarMenstruacaoHoje = async (req, res) => {
+    try{
+        const usuariaId = req.usuariaId;
+
+        //controller delega tudo pro service.
+        const resultado = await cicloService.alternarMenstruacaoHoje(usuariaId);
+
+        return res.status(200).json(resultado);
+    } catch (erro) {
+        return res.status(500).json({
+            erro: "ERRO_ALTERNAR_MENSTRUACAO_HOJE"
+        });
+    }
+};
+
+module.exports = {
+    obterDadosHome,
+    alternarMenstruacaoHoje
+}
