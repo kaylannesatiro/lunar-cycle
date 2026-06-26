fab51f65a65edc73dc52154b59e4dba0bcf5dafa
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-26 08:54:18 -0300

    criacao do repository diaMenstruacao, e criacao da função buscarUltimaMenstruacao

---
+++ backend/repositories/diaMenstruacaoRepository.js
@@ -0,0 +1,24 @@
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+// Buscar a data mais recente de menstruação de uma usuária
+const buscarUltimaMenstruacao = async (usuariaId) => {
+    return await prisma.diaMenstruacao.findFirst({
+        where: {usuariaId},
+        orderBy: {data: 'desc'}
+    })
+}
+
+// Verifica se um dia especifico está marcado (para a Home saber o status do botao)
+/*const verificarDiaMarcado = async (usuariaId, data) => {
+    const dia = await prisma.diaMenstruacao.findUnique({
+        where:{
+            usuariaId_data:{
+                usuariaId,
+                data
+            }
+        }
+    });
+    return dia !== null;
+};*/
