8b5a11cc9926de6999ffb967890d5d28334a8257
Kaylanne <kaylannesatiro@icloud.com>
2026-06-25 21:23:34 -0300

    style: correção css

--- frontend/src/components/features/Ciclo/DadosExtras.jsx
+++ frontend/src/components/features/Ciclo/DadosExtras.jsx
@@ -10,7 +10,9 @@ const DadosExtras = () => {
             {/* Dia do Ciclo Lunar */}
             <div className="dados-extras-card">
                 <div className="dados-extras-dado">
-                    <span className="dados-extras-valor">{metricas.diaDoCicloLunar}dia</span>
+                    <span className="dados-extras-valor">{metricas.diaDoCicloLunar}
+                        <span className="dados-extras-sufixo">dia</span>
+                    </span>
                 </div>
                 <span className="dados-extras-legenda">CICLO LUNAR</span>
             </div>
@@ -18,7 +20,9 @@ const DadosExtras = () => {
             {/* Próxima Lua Cheia */}
             <div className="dados-extras-card">
                 <div className="dados-extras-dado">
-                    <span className="dados-extras-valor">{metricas.faltamParaCheia}dias</span>
+                    <span className="dados-extras-valor">{metricas.faltamParaCheia}
+                        <span className="dados-extras-sufixo">dias</span>
+                    </span>
                 </div>
                 <span className="dados-extras-legenda">PRÓXIMA LUA CHEIA</span>
             </div>
@@ -26,7 +30,9 @@ const DadosExtras = () => {
             {/* Energia Lunar */}
             <div className="dados-extras-card">
                 <div className="dados-extras-dado">
-                    <span className="dados-extras-valor">{metricas.energia}%</span>
+                    <span className="dados-extras-valor">{metricas.energia}
+                        <span className="dados-extras-sufixo">%</span>
+                    </span>
                 </div>
                 <span className="dados-extras-legenda">ENERGIA LUNAR</span>
             </div>
