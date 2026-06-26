8980b8430457f9110c02bc778ff391ad92d04966
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-26 11:11:47 -0300

    fix: substitui instância local do PrismaClient pela instância compartilhada do config/prisma.js

--- backend/repositories/diaMenstruacaoRepository.js
+++ backend/repositories/diaMenstruacaoRepository.js
@@ -1,5 +1,4 @@
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../config/prisma');
 
 
 // Buscar a data mais recente de menstruação de uma usuária
