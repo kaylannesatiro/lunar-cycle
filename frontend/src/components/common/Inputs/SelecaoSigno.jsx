401538205a93f4730a5f5b44c718b72f5dad24bb
Kaylanne <kaylannesatiro@icloud.com>
2026-06-27 17:23:19 -0300

    feat: import de icons do react e correção css

--- frontend/src/components/common/Inputs/SelecaoSigno.jsx
+++ frontend/src/components/common/Inputs/SelecaoSigno.jsx
@@ -1,19 +1,23 @@
+import { 
+    TbZodiacAries, TbZodiacTaurus, TbZodiacGemini, TbZodiacCancer, TbZodiacLeo, TbZodiacVirgo, TbZodiacLibra, TbZodiacScorpio, TbZodiacSagittarius, TbZodiacCapricorn, TbZodiacAquarius, TbZodiacPisces 
+} from "react-icons/tb"
+
 import "./SelecaoSigno.css"
 
 const signosAstrologicos = [
-    { id: 'aries', icone: '♈', nome: 'Áries' },
-    { id: 'touro', icone: '♉', nome: 'Touro' },
-    { id: 'gemeos', icone: '♊', nome: 'Gêmeos' },
-    { id: 'cancer', icone: '♋', nome: 'Câncer' },
-    { id: 'leao', icone: '♌', nome: 'Leão' },
-    { id: 'virgem', icone: '♍', nome: 'Virgem' },
-    { id: 'libra', icone: '♎', nome: 'Libra' },
-    { id: 'escorpiao', icone: '♏', nome: 'Escorpião' },
-    { id: 'sagitario', icone: '♐', nome: 'Sagitário' },
-    { id: 'capricornio', icone: '♑', nome: 'Capricórnio' },
-    { id: 'aquario', icone: '♒', nome: 'Aquário' },
-    { id: 'peixes', icone: '♓', nome: 'Peixes' }
-];
+    { id: 'aries', icone: <TbZodiacAries />, nome: 'Áries' },
+    { id: 'touro', icone: <TbZodiacTaurus />, nome: 'Touro' },
+    { id: 'gemeos', icone: <TbZodiacGemini />, nome: 'Gêmeos' },
+    { id: 'cancer', icone: <TbZodiacCancer />, nome: 'Câncer' },
+    { id: 'leao', icone: <TbZodiacLeo />, nome: 'Leão' },
+    { id: 'virgem', icone: <TbZodiacVirgo />, nome: 'Virgem' },
+    { id: 'libra', icone: <TbZodiacLibra />, nome: 'Libra' },
+    { id: 'escorpiao', icone: <TbZodiacScorpio />, nome: 'Escorpião' },
+    { id: 'sagitario', icone: <TbZodiacSagittarius />, nome: 'Sagitário' },
+    { id: 'capricornio', icone: <TbZodiacCapricorn />, nome: 'Capricórnio' },
+    { id: 'aquario', icone: <TbZodiacAquarius />, nome: 'Aquário' },
+    { id: 'peixes', icone: <TbZodiacPisces />, nome: 'Peixes' }
+]
 
 const SelecaoSigno = ({value, onChange}) => {
 
