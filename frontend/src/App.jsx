10282a6d5f15f71b1bc2919f106e999fe505190c
Kaylanne <kaylannesatiro@icloud.com>
2026-06-27 15:37:16 -0300

    fix: correção app pra contemplar conta

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import Diario from './pages/Diario'
 import Conta from './pages/Conta'
@@ -6,18 +6,17 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="home" element={<h1>Home</h1>} />
-          <Route path="diario" element={<Diario/>} />
-          <Route path="conta" element={<Conta/>} />
-        </Route>
-      </Routes>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Navigate to="/home" replace />} />
+            <Route path="home" element={<h1>Home</h1>} />
+            <Route path="diario" element={<Diario/>} />
+            <Route path="conta" element={<Conta/>} />
+          </Route>
+        </Routes>
     </BrowserRouter>
-    </>
-  );
+  )
 }
 
 export default App;
